ec5c9d4587d1dd243b85b70b9932c17f6cc2221a
magnum <jimmy.w.larsson@gmail.com>
2025-06-25 17:20:24 +0200

    hello world template

--- src/main.rs
+++ src/main.rs
@@ -22,16 +22,6 @@ use maud::{DOCTYPE, Markup, html};
 use serde::Deserialize;
 use tower_http::services::ServeDir;
 
-// TODO:
-// - [x] Lets try the JS execution
-// - [x] wth is signals o
-//
-// what to they mean by this?
-// Signals beginning with an underscore are considered local signals and are not included in
-// requests to the --> backend by default <-- You can include them by setting the includeLocal option to
-// true.
-// https://data-star.dev/reference/attribute_plugins#data-signals
-
 macro_rules! sse_stream {
     ($($e:expr),*) => {
         Sse(stream!(
@@ -58,12 +48,19 @@ async fn main() {
 }
 
 async fn looper() -> impl IntoResponse {
+    const MSG: &'static str = "Hello World!";
     Sse(stream! {
-        for i in 0..10 {
-            yield MergeFragments::new(html!(p {(i)})).merge_mode(FragmentMergeMode::Append).selector("#target");
-            tokio::time::sleep(std::time::Duration::from_millis(200)).await;
+        for i in 0..MSG.len() {
+            yield MergeFragments::new(html!(p class="text-3xl" {(MSG[..i])})).merge_mode(FragmentMergeMode::Inner).selector("#target").into_event();
+            tokio::time::sleep(std::time::Duration::from_millis(150)).await;
         }
-
+        yield MergeFragments::new(html!(p class="text-3xl" {({ 
+            let mut s = MSG.to_string();
+            s.push_str("👋"); 
+            s
+        })}
+        )).merge_mode(FragmentMergeMode::Inner).selector("#target").into_event();
+        yield MergeSignals::new("{sent: false}").into_event()
     })
 }
 
@@ -71,18 +68,25 @@ async fn mainpage() -> Markup {
     html!(
         (DOCTYPE)
         head {
-        title {"Hello world!"}
+            title {"Hello world!"}
             script type="module" src="/static/datastar.js" {}
             script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" {}
         }
 
         body {
-            button data-on-click="@get('/test')" {"Press me"}
-            div {
-                input data-bind-myvar placeholder="write shit.." {}
-                div data-text="$myvar" {}
-            }
-            div class="flex flex-row gap-4" id="target" {}
+            div class="flex bg-slate-50 flex-col items-center justify-between h-screen w-screen"{
+                div {}
+                div data-signals="{sent : false}" class="flex flex-col items-center h-20 gap-2" {
+                    button #hello type="button"
+                        data-attr="{ disabled : $sent}"
+                        data-on-click="@get('/test'); $sent = true;"
+                        data-class="{ 'hover:bg-slate-200' : !$sent, 'hover:bg-red-50': $sent }"
+                        class="border p-2 border-slate-800 rounded-md"
+                        {"Press me"}
+                    div class="" id="target" {}
+                }
+                div {}
+                }
             }
     )
 }
